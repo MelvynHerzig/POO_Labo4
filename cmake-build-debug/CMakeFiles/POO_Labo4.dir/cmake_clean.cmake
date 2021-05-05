f892fa5be95fec13c2bb1ffe218529f175e87af6
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-05-05 15:09:23 +0200

    Init

---
+++ cmake-build-debug/CMakeFiles/POO_Labo4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO_Labo4.dir/main.cpp.obj"
+  "POO_Labo4.exe"
+  "POO_Labo4.exe.manifest"
+  "POO_Labo4.pdb"
+  "libPOO_Labo4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO_Labo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
